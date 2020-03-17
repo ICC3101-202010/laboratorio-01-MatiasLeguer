7f155dc4d1ac9f0ba245309cc55d211b8e2c9613
MatiasLeguer <62114519+MatiasLeguer@users.noreply.github.com>
2020-03-17 18:20:16 -0300

    Parte 1: Entrega un numero random del 0 al 2

---
+++ piedra_papel_o_tijera/Cachipun/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cachipun
+{
+    public class Person
+    {
+        public string name;
+        public string surname;
+
+
+        public void Play()
+        {
+            Random r = new Random();
+            int random = r.Next(0, 3);
+            Console.WriteLine(random);
+        }
+
+
+    }
+}
---
+++ piedra_papel_o_tijera/Cachipun/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cachipun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Play();
+
+        }
+    }
+}
+
